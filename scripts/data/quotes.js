64d23025e7abad4295f3a2d7cab46f58b4cd6d8b
ofentse <ofentsebotlhale88@outlook.com>
2024-06-09 21:55:38 +0200

    added quotes and randomized them

---
+++ scripts/data/quotes.js
@@ -0,0 +1,30 @@
+export const quotes = [
+  {
+    text: "Whether you think you can or you think you can't, you're right.",
+    author: "Henry Ford",
+  },
+  {
+    text: "You miss 100% of the shots you don't take.",
+    author: "Wayne Gretzky",
+  },
+  {
+    text: "It does not matter how slowly you go as long as you do not stop.",
+    author: "Confucius",
+  },
+  {
+    text: "I would rather die of passion than of boredom.",
+    author: "Vincent Van Gogh",
+  },
+  {
+    text: "There is no substitute for hard work.",
+    author: "Thomas Edison",
+  },
+  {
+    text: "When I see your face, there’s not a thing that I would change, ’cause you’re amazing – just the way you are.",
+    author: "Me",
+  },
+  {
+    text: "Because of you, I can feel myself slowly, but surely, becoming the me I have always dreamed of being.",
+    author: "Your lover",
+  },
+];
--- scripts/home.js
+++ scripts/home.js
@@ -1,14 +1,16 @@
 //i need to create a section that will display over the
 //page saying the page is still undergoing production
+import { quotes } from "./data/quotes.js";
 
+let quote = quotes[Math.floor(Math.random() * quotes.length)];
 let day = new Date().toTimeString().split(" ")[0];
 let hour = day[0] + day[1];
 let homeHTML = `
     <div
-        class="w-full flex-col text-center flex items-center justify-center h-full"
+        class=" w-full flex-col text-center flex items-center justify-center h-full"
     >
-        <p class="bg-red-800 p-4 rounded-lg font-medium text-xl">
-            Good Morning Love
+        <p id='text-field' class="flex flex-col bg-red-800 p-4 rounded-lg font-medium text-xl">
+            ${quote.text}<span class='text-sm'> ~ ${quote.author}</span>
         </p>
         <img
             class="mt-20 animate-ping"
@@ -39,8 +41,8 @@ let homeHTML = `
     </div>
 `;
 let windowElement = document.getElementById("window");
-//alert(hour);
+//console.log(quote.text, quote.author);
 
-if (hour < 12) {
+if (hour > 12) {
   windowElement.innerHTML = homeHTML;
 }
